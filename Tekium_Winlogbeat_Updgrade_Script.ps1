b4d62e68c5d42c8fb0feb2369f8f9b157275c013
rerick38 <erickrr.tbd93@gmail.com>
2023-09-12 11:14:31 -0600

    Version 1.3
    New version

--- Tekium_Winlogbeat_Updgrade_Script.ps1
+++ Tekium_Winlogbeat_Updgrade_Script.ps1
@@ -1,139 +1,133 @@
-﻿Param([string] $hosts_file_path = "C:\")
+﻿<#
+.Description
+The script updates the Winlogbeat agent using the domain controller and the winRM service.
+.PARAMETER hosts_file_name
+Name of the TXT file that contains the names of the Windows servers where Winlogbeat will be updated.
+It is an optional value.
+.EXAMPLE
+PS> .\Tekium_Winlogbeat_Updgrade_Script.ps1 -hosts_file_name "hosts_file_update.txt"
+.SYNOPSIS
+Powershell script to update Winlogbeat on Windows systems.
+#>
+Param([string] $hosts_file_name = "hosts_update.txt")
 Clear-Host
 
-$dir_actual = (pwd).ToString()
-$file_hosts = $dir_actual + "\hosts_update.txt"
-$log = $dir_actual + "\tekium_winlog_upgrade_script_log.log"
-$from = $dir_actual + "\winlogbeat"
+$current_date = Get-Date -Format "yyyy_MM_dd"
+$log_file_name = "winlogbeat_update_$current_date.log"
 
 Write-Host -Object "-------------------------------------------------------------------------------------" -ForegroundColor Yellow
-write-host -Object "Copyright©Tekium 2023. All rights reserved." -ForegroundColor green
-write-host -Object "Author: Erick Roberto Rodríguez Rodríguez" -ForegroundColor green
-write-host -Object "Email: erodriguez@tekium.mx, erickrr.tbd93@gmail.com" -ForegroundColor green
-Write-Host -Object "GitHub: https://github.com/erickrr-bd/Tekium-PAN-Hunter-Script" -ForegroundColor green
-write-host -Object "Tekium Winlogbeat Upgrade Script v1.3 - September 2023" -ForegroundColor green
+Write-host -Object "Copyright©Tekium 2023. All rights reserved." -ForegroundColor green
+Write-host -Object "Author: Erick Roberto Rodríguez Rodríguez" -ForegroundColor green
+Write-host -Object "Email: erodriguez@tekium.mx, erickrr.tbd93@gmail.com" -ForegroundColor green
+Write-Host -Object "GitHub: https://github.com/erickrr-bd/Tekium-Winlogbeat-Update-Script" -ForegroundColor green
+Write-host -Object "Tekium Winlogbeat Upgrade Script v1.3 - September 2023" -ForegroundColor green
 Write-Host -Object "-------------------------------------------------------------------------------------" -ForegroundColor Yellow
-Write-Output -InputObject "Hosts File Path: $hosts_file_path`n"
+Write-Output -InputObject "Hosts File Name: $hosts_file_name`n"
 
-$hosts_list_to_update = Get-Content hosts_file_path
-
-foreach($host_to_update in $hosts_list_to_update){
-    
+$hosts_list = Get-Content -Path $hosts_file_name -ErrorAction SilentlyContinue -ErrorVariable Err
+if(-not $?){
+    Write-Host -Object "`nFile not found: $hosts_file_name" -ForegroundColor Red
+    (Get-Date).ToString() + " Error: $Err[0]" | Out-File -FilePath $log_file_name -Append
 }
-
-<#
-foreach($host_update in $list_hosts_update){
-     $flag_service = 0
-     $flag_directory = 0
-     $flag_copy = 0
-     $flag_not_winlog = 0
-     $flag_winlog = 0
-     $winrm_validation = Test-WSMan -ComputerName $host_update -ErrorAction SilentlyContinue -ErrorVariable Err
-     $winlogbeat_service = Get-Service -ComputerName $host_update winlogbeat -ErrorAction SilentlyContinue -ErrorVariable Err
-     $path_winlogbeat = '\\' + $host_update + '\c$\Program Files\winlogbeat'
-     $to_path = '\\' + $host_update + '\c$\Program Files'
-     if($winrm_validation){
-        Write-Host "`nConnection established with the computer: $host_update" -ForegroundColor Green
-        (Get-Date).ToString() + " " + "Connection established with the computer: $host_update" | Out-File -FilePath $log -Append
-        if($winlogbeat_service){
-            Write-Host "`nStopping winlogbeat service on the computer: $host_update" -ForegroundColor Green
-            (Get-Date).ToString() + " " + "Stopping winlogbeat service on the computer: $host_update" | Out-File -FilePath $log -Append
-            $winlogbeat_service | Set-Service -Status Stopped
-            Start-Sleep -s 5
-            $status_service_stop = $winlogbeat_service.status
-            Write-Host "`nCurrent status of the Winlogbeat service on the computer: $host_update" -ForegroundColor Green
-            (Get-Date).ToString() + " " + "Current status of the Winlogbeat service on the computer: $host_update" | Out-File -FilePath $log -Append
-            Write-Host "Winlogbeat service status: $status_service_stop" -ForegroundColor Yellow
-            (Get-Date).ToString() + " " + "Winlogbeat service status: $status_service_stop" | Out-File -FilePath $log -Append
-            if($status_service_stop -like "Stopped"){
-                $flag_service = 1
+else{
+    "-------------------------------------------------------------------------------------" | Out-File -FilePath $log_file_name -Append
+    "Copyright©Tekium 2023. All rights reserved." | Out-File -FilePath $log_file_name -Append
+    "Author: Erick Roberto Rodríguez Rodríguez" | Out-File -FilePath $log_file_name -Append
+    "Email: erodriguez@tekium.mx, erickrr.tbd93@gmail.com" | Out-File -FilePath $log_file_name -Append
+    "GitHub: https://github.com/erickrr-bd/Tekium-Winlogbeat-Update-Script" | Out-File -FilePath $log_file_name -Append
+    "Tekium Winlogbeat Upgrade Script v1.3 - September 2023" | Out-File -FilePath $log_file_name -Append
+    "-------------------------------------------------------------------------------------" | Out-File -FilePath $log_file_name -Append
+    "Hosts File Name: $hosts_file_name" | Out-File -FilePath $log_file_name -Append
+    foreach($hostname in $hosts_list){
+        $winrm_validation = Test-WSMan -ComputerName $hostname -ErrorAction SilentlyContinue -ErrorVariable Err
+        if($winrm_validation){
+            Write-Host -Object "`nConnection established with the server: $hostname" -ForegroundColor Green
+            (Get-Date).ToString() + " Connection established with the server: $hostname" | Out-File -FilePath $log_file_name -Append
+            Start-Sleep -s 2
+            $winlogbeat_service = Get-Service -ComputerName $hostname -Name winlogbeat -ErrorAction SilentlyContinue -ErrorVariable Err
+            if($winlogbeat_service){
+                Write-Host -Object "`nStopping winlogbeat service on the server: $hostname" -ForegroundColor Green
+                (Get-Date).ToString() + " Stopping winlogbeat service on the server: $hostname" | Out-File -FilePath $log_file_name -Append
+                Start-Sleep -s 2
+                $winlogbeat_service | Set-Service -Status Stopped
+                Start-Sleep -s 2
+                $winlogbeat_service_status = $winlogbeat_service.status
+                Write-Host -Object "`nWinlogbeat service status: $winlogbeat_service_status on server $hostname" -ForegroundColor Green
+                (Get-Date).ToString() + " Winlogbeat service status: $winlogbeat_service_status on server $hostname" | Out-File -FilePath $log_file_name -Append
             }
-            $flag_winlog = 1
-        }
-        else{
-            $flag_service = 1
-            $flag_not_winlog = 1
-             Write-Host "`nWinlogbeat service not found on the computer: $host_update" -ForegroundColor Red
-            (Get-Date).ToString() + " " + "Winlogbeat service not found on the computer: $host_update" | Out-File -FilePath $log -Append
-            (Get-Date).ToString() + " " + "Error: $Err[0]" | Out-File -FilePath $log -Append
-            Start-Sleep -s 5
-        }
-        if($flag_service -eq 1){
-            if(Test-Path $path_winlogbeat){
-                Remove-Item $path_winlogbeat -Force -Recurse -ErrorAction SilentlyContinue -ErrorVariable Err
-                Start-Sleep -s 5
-                if(-not $?){
-                    Write-Host "`nError removing the previous version of Winlogbeat on the computer: $host_update" -ForegroundColor Red
-                    (Get-Date).ToString() + " " + "Error removing the previous version of Winlogbeat on the computer: $host_update" | Out-File -FilePath $log -Append
-                    (Get-Date).ToString() + " " + "Error: $Err[0]" | Out-File -FilePath $log -Append
+            else{
+                Write-Host -Object "`nWinlogeat service not found on server: $hostname" -ForegroundColor Red
+                (Get-Date).ToString() + " Winlogeat service not found on server: $hostname" | Out-File -FilePath $log_file_name -Append
+                (Get-Date).ToString() + " Error: $Err[0]" | Out-File -FilePath $log_file_name -Append
+                Start-Sleep -s 2
+                Write-Host -Object "`nCreating Winlogbeat service on the server: $hostname" -ForegroundColor Green
+                (Get-Date).ToString() + " Creating Winlogbeat service on the server: $hostname" | Out-File -FilePath $log_file_name -Append
+                Start-Sleep -s 2
+                Invoke-Command -ComputerName $hostname -ScriptBlock{
+                    $workdir = "C:\Program Files\winlogbeat"
+	                New-Service -name winlogbeat `
+                        -displayName Winlogbeat `
+                        -binaryPathName "`"$workdir\winlogbeat.exe`" --environment=windows_service -c `"$workdir\winlogbeat.yml`" --path.home `"$workdir`" --path.data `"$env:PROGRAMDATA\winlogbeat`" --path.logs `"$env:PROGRAMDATA\winlogbeat\logs`" -E logging.files.redirect_stderr=true"
+                } -ErrorAction SilentlyContinue -ErrorVariable Err
+                if($Err){
+	                Write-Host -Object "`nWinlogbeat service not created on server: $hostname" -ForegroundColor Red
+                    (Get-Date).ToString() + " Winlogbeat service not created on server: $hostname" | Out-File -FilePath $log_file_name -Append
+	                (Get-Date).ToString() + " Error: $Err" | Out-File -FilePath $log_file_name -Append
                 }
                 else{
-                    $flag_directory = 1
-                    Write-Host "`nPrevious version of Winlogbeat removed on the computer: $host_update" -ForegroundColor Green
-                    (Get-Date).ToString() + " " + "Previous version of Winlogbeat removed on the computer: $host_update" | Out-File -FilePath $log -Append
+                    Write-Host -Object "`nWinlogbeat service created on the server: $hostname" -ForegroundColor Green
+                    (Get-Date).ToString() + " Winlogbeat service created on server: $hostname" | Out-File -FilePath $log_file_name -Append
+                }
+
+            }
+            Start-Sleep -s 2
+            $winlogbeat_path = "\\" + $hostname + "\c$\Program Files\winlogbeat"
+            $program_files_path = "\\" + $hostname + "\c$\Program Files"
+            if(Test-Path $winlogbeat_path){
+                Write-Host -Object "`nPrevious version of Winlogbeat found on the server: $hostname" -ForegroundColor Green
+                (Get-Date).ToString() + " Previous version of Winlogbeat found on the server: $hostname" | Out-File -FilePath $log_file_name -Append
+                Start-Sleep -s 2
+                Remove-Item $winlogbeat_path -Force -Recurse -ErrorAction SilentlyContinue -ErrorVariable Err
+                Start-Sleep -s 2
+                if(-not $?){
+                    Write-Host -Object "`nCurrent winlogbeat not deleted on server: $hostname" -ForegroundColor Red
+                    (Get-Date).ToString() + " Current winlogbeat not deleted on server: $hostname" | Out-File -FilePath $log_file_name -Append
+                    (Get-Date).ToString() + " Error: $Err[0]" | Out-File -FilePath $log_file_name -Append
+                }
+                else{
+                    Write-Host -Object "`nCurrent winlogbeat deleted on server: $hostname" -ForegroundColor Green
+                    (Get-Date).ToString() + " Current winlogbeat deleted on server: $hostname" | Out-File -FilePath $log_file_name -Append
                 }
             }
-            else{
-                $flag_directory = 1
-                Write-Host "`nThe Winlogbeat directory was not found on the computer: $host_update" -ForegroundColor Red
-                (Get-Date).ToString() + " " + "The Winlogbeat directory was not found on the computer: $host_update" | Out-File -FilePath $log -Append
-            }
-        }
-        if($flag_directory -eq 1){
-            Copy-Item -Path $from -Destination $to_path -Recurse -Force -ErrorAction SilentlyContinue -ErrorVariable Err
-            Start-Sleep -s 5
+            Start-Sleep -s 2
+            Write-Host -Object "`nCopying new version of Winlogbeat to the server: $hostname" -ForegroundColor Green
+            (Get-Date).ToString() + " Copying new version of Winlogbeat to the server: $hostname" | Out-File -FilePath $log_file_name -Append
+            Start-Sleep -s 2
+            Copy-Item -Path "winlogbeat" -Destination $program_files_path -Recurse -Force -ErrorAction SilentlyContinue -ErrorVariable Err
+            Start-Sleep -s 2
             if(-not $?){
-                Write-Host "`nError copying new version of Winlogbeat on the computer: $host_update" -ForegroundColor Red
-                (Get-Date).ToString() + " " + "Error copying new version of Winlogbeat on the computer: $host_update" | Out-File -FilePath $log -Append
-                (Get-Date).ToString() + " " + "Error: $Err[0]" | Out-File -FilePath $log -Append
+                Write-Host -Object "`nWinlogbeat not copied to server: $hostname" -ForegroundColor Red
+                (Get-Date).ToString() + " Winlogbeat not copied to server: $hostname" | Out-File -FilePath $log_file_name -Append
+                (Get-Date).ToString() + " Error: $Err[0]" | Out-File -FilePath $log_file_name -Append
             }
             else{
-                $flag_copy = 1
-                Write-Host "`nNew version of Winlogbeat installed on the computer: $host_update" -ForegroundColor Green
-                (Get-Date).ToString() + " " + "New version of Winlogbeat installed on the computer: $host_update" | Out-File -FilePath $log -Append
+                Write-Host -Object "`nWinlogbeat copied to server: $hostname" -ForegroundColor Green
+                (Get-Date).ToString() + " Winlogbeat copied to server: $hostname" | Out-File -FilePath $log_file_name -Append
             }
-        }
-        if($flag_copy -eq 1 -and $flag_not_winlog -eq 1){
-            Write-Host "`nCreating the Winlogbeat service on the computer: $host_update" -ForegroundColor Green
-            (Get-Date).ToString() + " " + "Creating the Winlogbeat service on the computer: $host_update" | Out-File -FilePath $log -Append
-            Invoke-Command -ComputerName $host_update -ScriptBlock{
-                $workdir = "C:\Program Files\winlogbeat"
-	            New-Service -name winlogbeat `
-                    -displayName Winlogbeat `
-                    -binaryPathName "`"$workdir\winlogbeat.exe`" --environment=windows_service -c `"$workdir\winlogbeat.yml`" --path.home `"$workdir`" --path.data `"$env:PROGRAMDATA\winlogbeat`" --path.logs `"$env:PROGRAMDATA\winlogbeat\logs`" -E logging.files.redirect_stderr=true"
-            } -ErrorAction SilentlyContinue -ErrorVariable Err
-            if($Err){
-	            write-host "Error: $Err" -ForegroundColor Red
-	            (Get-Date).ToString() + " " + "Error: $Err" | Out-File -FilePath $log -Append
-            }
-            $winlogbeat_service = Get-Service -ComputerName $host_update winlogbeat -ErrorAction SilentlyContinue -ErrorVariable Err
-            if($winlogbeat_service){
-                $flag_winlog = 1
-                Write-Host "`nWinlogbeat service created on the computer: $host_update" -ForegroundColor Green
-                (Get-Date).ToString() + " " + "Winlogbeat service created on the computer: $host_update" | Out-File -FilePath $log -Append
-            }
-            else{
-                Write-Host "`nError creating Winlogbeat service on computer: $host_update" -ForegroundColor Red
-                (Get-Date).ToString() + " " + "Error creating Winlogbeat service on computer: $host_update" | Out-File -FilePath $log -Append
-            }
-        }
-        if($flag_winlog -eq 1){
-            Write-Host "`nStarting Winlogbeat service on the computer: $host_update" -ForegroundColor Green
-            (Get-Date).ToString() + " " + "Starting Winlogbeat service on the computer: $host_update" | Out-File -FilePath $log -Append
+            Start-Sleep -s 2
+            Write-Host -Object "`nStarting the Winlogbeat service on the server: $hostname" -ForegroundColor Green
+            (Get-Date).ToString() + " Starting the Winlogbeat service on the server: $hostname" | Out-File -FilePath $log_file_name -Append
+            Start-Sleep -s 2
             $winlogbeat_service | Set-Service -Status Running
-            Start-Sleep -s 5
-            $status_service_start = $winlogbeat_service.status
-            Write-Host "`nCurrent status of the Winlogbeat service on the computer: $host_update" -ForegroundColor Green
-            (Get-Date).ToString() + " " + "Current status of the Winlogbeat service on the computer: $host_update" | Out-File -FilePath $log -Append
-            Write-Host "Winlogbeat service status: $status_service_start" -ForegroundColor Yellow
-            (Get-Date).ToString() + " " + "Winlogbeat service status: $status_service_start" | Out-File -FilePath $log -Append
+            Start-Sleep -s 2
+            $winlogbeat_service_status = $winlogbeat_service.status
+            Write-Host -Object "`nWinlogbeat service status: $winlogbeat_service_status on server $hostname`n" -ForegroundColor Green
+            (Get-Date).ToString() + " Winlogbeat service status: $winlogbeat_service_status on server $hostname" | Out-File -FilePath $log_file_name -Append
         }
-     }
-     else{
-        Write-Host "`nError connecting to the computer: $host_update" -ForegroundColor Red
-        (Get-Date).ToString() + " " + "Error connecting to the computer: $host_update" | Out-File -FilePath $log -Append
-        (Get-Date).ToString() + " " + "Error: $Err[0]" | Out-File -FilePath $log -Append
-        Start-Sleep -s 5 
-     }
+        else{
+            Write-Host -Object "`nFailed to connect to server: $hostname" -ForegroundColor Red
+            (Get-Date).ToString() + " Failed to connect to server: $hostname" | Out-File -FilePath $log_file_name -Append
+           (Get-Date).ToString() + " Error: $Err[0]" | Out-File -FilePath $log_file_name -Append
+        }
+    }
 }
-#>
